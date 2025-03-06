765c6ddfa468a3e064fb2f55f2c530367401e153
Dimitri <22354833+dimitriaugusto@users.noreply.github.com>
2025-03-06 19:30:16 -0300

    Adds customer events and handlers.

--- src/domain/@shared/event/event-dispatcher.spec.ts
+++ src/domain/@shared/event/event-dispatcher.spec.ts
@@ -1,3 +1,8 @@
+import AddressChangedEvent from "../../customer/event/address-changed/address-changed-event";
+import EnviaConsoleLogHandler from "../../customer/event/address-changed/handler/envia-console-log-handler";
+import CustomerCreatedEvent from "../../customer/event/customer-created/customer-created-event";
+import EnviaConsoleLog1Handler from "../../customer/event/customer-created/handler/envia-console-log1-handler";
+import EnviaConsoleLog2Handler from "../../customer/event/customer-created/handler/envia-console-log2-handler";
 import SendEmailWhenProductIsCreatedHandler from "../../product/event/handler/send-email-when-product-is-created.handler";
 import ProductCreatedEvent from "../../product/event/product-created.event";
 import EventDispatcher from "./event-dispatcher";
@@ -57,6 +62,33 @@ describe("Domain events tests", () => {
     ).toBeUndefined();
   });
 
+  it("should unregister all event handlers with multiple events", () => {
+    const eventDispatcher = new EventDispatcher();
+    const eventHandlerProduct = new SendEmailWhenProductIsCreatedHandler();
+    const eventHandlerCustomer = new EnviaConsoleLog1Handler();
+
+    eventDispatcher.register("ProductCreatedEvent", eventHandlerProduct);
+    eventDispatcher.register("CustomerCreatedEvent", eventHandlerCustomer);
+
+    expect(
+      eventDispatcher.getEventHandlers["ProductCreatedEvent"][0]
+    ).toMatchObject(eventHandlerProduct);
+
+    expect(
+      eventDispatcher.getEventHandlers["CustomerCreatedEvent"][0]
+    ).toMatchObject(eventHandlerCustomer);
+
+    eventDispatcher.unregisterAll();
+
+    expect(
+      eventDispatcher.getEventHandlers["ProductCreatedEvent"]
+    ).toBeUndefined();
+    expect(
+      eventDispatcher.getEventHandlers["CustomerCreatedEvent"]
+    ).toBeUndefined();
+
+  });
+
   it("should notify all event handlers", () => {
     const eventDispatcher = new EventDispatcher();
     const eventHandler = new SendEmailWhenProductIsCreatedHandler();
@@ -79,4 +111,55 @@ describe("Domain events tests", () => {
 
     expect(spyEventHandler).toHaveBeenCalled();
   });
+
+  it("should notify all event handlers of CustomerCreatedEvent", () => {
+    const eventDispatcher = new EventDispatcher();
+    const eventHandler1 = new EnviaConsoleLog1Handler();
+    const eventHandler2 = new EnviaConsoleLog2Handler();
+    const spyEventHandler1 = jest.spyOn(eventHandler1, "handle");
+    const spyEventHandler2 = jest.spyOn(eventHandler2, "handle");
+
+    eventDispatcher.register("CustomerCreatedEvent", eventHandler1);
+    eventDispatcher.register("CustomerCreatedEvent", eventHandler2);
+
+    expect(
+      eventDispatcher.getEventHandlers["CustomerCreatedEvent"][0]
+    ).toMatchObject(eventHandler1);
+    expect(
+      eventDispatcher.getEventHandlers["CustomerCreatedEvent"][1]
+    ).toMatchObject(eventHandler2);
+
+    const createdEvent = new CustomerCreatedEvent({
+      name: "Customer 1",
+    });
+
+    eventDispatcher.notify(createdEvent);
+
+    expect(spyEventHandler1).toHaveBeenCalled();
+    expect(spyEventHandler2).toHaveBeenCalled();
+  });
+
+  it("should notify all event handlers of AddressChangedEvent", () => {
+    const eventDispatcher = new EventDispatcher();
+    const eventHandler = new EnviaConsoleLogHandler();
+    const spyEventHandler = jest.spyOn(eventHandler, "handle");
+
+    eventDispatcher.register("AddressChangedEvent", eventHandler);
+
+    expect(
+      eventDispatcher.getEventHandlers["AddressChangedEvent"][0]
+    ).toMatchObject(eventHandler);
+
+    const addressChangedEvent = new AddressChangedEvent({
+      id: 1234,
+      name: "Cliente 2",
+      address: "999, Any Street, Any City, AnyZip",
+    });
+
+    eventDispatcher.notify(addressChangedEvent);
+
+    expect(spyEventHandler).toHaveBeenCalled();
+  });
+
 });
+
